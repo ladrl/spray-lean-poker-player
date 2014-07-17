adf9e91b365dc06861ed6e1eb0e9e60ea9cac89b
laedracl <lukas.laedrach@zuehlke.com>
2014-07-17 17:10:37 +0200

    Make 'check' work.

--- src/main/scala/com/example/PokerPlayer.scala
+++ src/main/scala/com/example/PokerPlayer.scala
@@ -19,21 +19,15 @@ class MyServiceActor extends Actor with MyService {
   def receive = runRoute(myRoute)
 }
 
-
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
 
   val myRoute =
     path("") {
       get {
-        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
-          complete {
-            <html>
-              <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
-              </body>
-            </html>
-          }
+        parameter('action) {
+          case "check" => complete("We're here!")
+          case _ => complete("huh?")
         }
       }
     }
